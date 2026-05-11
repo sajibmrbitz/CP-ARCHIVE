8c60d4dd7b32cb686b10d88a6b9e73b0cd5e4652
sajibmrbitz <sajibmr2005@gmail.com>
2026-05-11 04:34:38 +0000

    Sync LeetCode submission Runtime - 3 ms (84.56%), Memory - 47.6 MB (94.08%)

---
+++ LeetCode/0621-task-scheduler/solution.java
@@ -0,0 +1,23 @@
+class Solution{
+    public int leastInterval(char[] tasks,int n){
+        int[] freq=new int[26];
+        int maxFreq=0;
+        
+        for(char task:tasks){
+            freq[task-'A']++;
+            maxFreq=Math.max(maxFreq,freq[task-'A']);
+        }
+        
+        int maxCount=0;
+        for(int count:freq){
+            if(count==maxFreq){
+                maxCount++;
+            }
+        }
+        
+        int intervals=(maxFreq-1)*(n+1)+maxCount;
+        
+        return Math.max(tasks.length,intervals);
+    }
+}
+
