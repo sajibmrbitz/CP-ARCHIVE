db34dffc2b79d9e0276f14e300131450a777d1e8
sajibmrbitz <sajibmr2005@gmail.com>
2026-05-18 06:44:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 7.9 MB (23.26%)

---
+++ LeetCode/0168-excel-sheet-column-title/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string convertToTitle(int columnNumber){
+        string ans = "";
+        
+        while(columnNumber>0){
+            columnNumber--;
+            int rem=columnNumber%26;
+            ans+=(char)('A'+rem);
+            columnNumber/=26;
+        }
+        // reverse order 
+        reverse(ans.begin(),ans.end());
+        
+        return ans;
+    }
+};
+
