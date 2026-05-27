7d01846c6458b906ac47a93c2691e75ec62f4229
sajibmrbitz <sajibmr2005@gmail.com>
2026-05-27 07:49:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 10 MB (26.68%)

---
+++ LeetCode/0067-add-binary/solution.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string addBinary(string a, string b){
+
+        string binsum="";
+        int i=a.size()-1;
+        int j=b.size()-1;
+        int carry=0;
+
+        while(i>=0 || j>=0 || carry>0){
+
+            int sum=carry;
+
+            if(i>=0){
+                sum+=(a[i]-'0');
+                i--;
+            }
+            if(j>=0){
+                sum+=(b[j]-'0');
+                j--;
+            }
+
+            binsum+=to_string(sum%2);
+            carry=sum/2;
+
+        }
+
+        reverse(binsum.begin(),binsum.end());
+        return binsum;
+    }
+};
