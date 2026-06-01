4f25106169229047b00f087cf86a72dc21cc9f09
sajibmrbitz <sajibmr2005@gmail.com>
2026-06-01 12:32:53 +0000

    Sync LeetCode submission Runtime - 738 ms (28.23%), Memory - 0.0B (100.00%)

---
+++ LeetCode/0184-department-highest-salary/solution.sql
@@ -0,0 +1,11 @@
+/* Write your PL/SQL query statement below */
+
+SELECT Department.name AS Department, Employee.name AS Employee, Employee.salary AS Salary
+FROM Employee
+JOIN Department ON Employee.departmentId=Department.id
+--multi column subquery
+WHERE (Employee.departmentId, Employee.salary) IN (
+    SELECT departmentId, MAX(salary)
+    FROM Employee
+    GROUP BY departmentId
+);
