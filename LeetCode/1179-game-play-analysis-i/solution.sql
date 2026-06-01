185c4d819cce2c8162131db70cfcc35c6f6ad4ec
sajibmrbitz <sajibmr2005@gmail.com>
2026-06-01 14:05:42 +0000

    Sync LeetCode submission Runtime - 541 ms (23.85%), Memory - 0.0B (100.00%)

--- LeetCode/1179-game-play-analysis-i/solution.sql
+++ LeetCode/1179-game-play-analysis-i/solution.sql
@@ -1,5 +1,6 @@
-/* Write your PL/SQL query statement below */
+/* Write your SQL query statement below */
 
-SELECT player_id, TO_CHAR(MIN(event_date), 'YYYY-MM-DD') AS first_login
+SELECT player_id, MIN(event_date) AS first_login
 FROM Activity
 GROUP BY player_id;
+
