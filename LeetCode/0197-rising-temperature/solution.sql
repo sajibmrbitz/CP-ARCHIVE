37c593969a0f711ec28cbe51395d6080006bcfd1
sajibmrbitz <sajibmr2005@gmail.com>
2026-06-01 12:45:49 +0000

    Sync LeetCode submission Runtime - 586 ms (26.13%), Memory - 0.0B (100.00%)

--- LeetCode/0197-rising-temperature/solution.sql
+++ LeetCode/0197-rising-temperature/solution.sql
@@ -1,7 +1,8 @@
 /* Write your PL/SQL query statement below */
+--self join to compare
 
 SELECT today.id
 FROM Weather today
-JOIN Weather yesterday ON DATEDIFF(today.recordDate, yesterday.recordDate)=1
+JOIN Weather yesterday ON (today.recordDate-yesterday.recordDate)=1
 WHERE today.temperature>yesterday.temperature;
 
